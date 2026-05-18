fec6597576e3d66cc7e40cf5c8ba3deec2823a24
twoseam <michael@twoseam.com>
2026-05-18 13:29:46 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -17,7 +17,7 @@
   ];
 
   // Bump this on every release; mirror it in the footer link + changelog.html.
-  var VERSION = 'v4.0.0';
+  var VERSION = 'v4.2.0';
 
   /* ===================== Theme (light / dark) =====================
      Default is dark. The user's choice is remembered in localStorage.
